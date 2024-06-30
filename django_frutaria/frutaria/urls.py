0e13d17e070a3cea925056372c3ccf12164c7d8f
Jeferson Campos <jeffbcampos@gmail.com>
2024-06-30 17:45:31 -0300

    add html

--- django_frutaria/frutaria/urls.py
+++ django_frutaria/frutaria/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import Index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', Index.as_view(), name='index'),
     path('create_user/', views.create_user, name='create_user'),
     path('create_fruit/', views.create_fruit, name='create_fruit'),
     path('sale/', views.sale, name='sale'),
--- django_frutaria/frutaria/views.py
+++ django_frutaria/frutaria/views.py
@@ -1,34 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Fruta, User, Vendas
 from bcrypt import hashpw, gensalt
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views import View
+from django.views.generic import TemplateView
 import json
 
-def index(request):
-    return HttpResponse('Olá, mundo!')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class Index(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+    def post(self, request):
+        usuario = request.POST.get('username')
+        senha = request.POST.get('password')
+        try:
+            user = User.objects.get(nome=usuario)
+        except User.DoesNotExist:
+            return HttpResponse('Usuário ou senha inválidos!')
+
+        if user.password == hashpw(senha.encode('utf-8'), user.password.encode('utf-8')):
+            if user.is_admin:
+                return redirect('admin')  # ajuste a URL name conforme necessário
+            else:
+                return redirect('vendedor')  # ajuste a URL name conforme necessário
+        else:
+            return HttpResponse('Usuário ou senha inválidos!')    
+
     
 @csrf_exempt
 def create_user(request):
-    data = json.loads(request.body)    
+    data = json.loads(request.body)
     nome = data.get('nome')
     password = data.get('password')
-    is_admin = data.get('is_admin')    
-    print(nome, password, is_admin)    
-    password = hashpw(password.encode('utf-8'), gensalt())
-    user = User(nome=nome, password=password, is_admin=is_admin)    
+    is_admin = data.get('is_admin', False)
+
+    hashed_password = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
+
+    user = User(nome=nome, password=hashed_password, is_admin=is_admin)
     user.save()
-    print(user)
-    return HttpResponse('Usuário criado com sucesso!')
+    
+    return JsonResponse({'message': 'Usuário criado com sucesso!'}, status=201)
+    
 
 @csrf_exempt
 def create_fruit(request):
-    nome = request.POST.get('nome')
-    classificacao = request.POST.get('classificacao')
-    fresca = request.POST.get('fresca')
-    qtd_estoque = request.POST.get('qtd_estoque')
-    preco = request.POST.get('preco')
+    data = json.loads(request.body)
+    nome = data.get('nome')
+    classificacao = data.get('classificacao')
+    fresca = data.get('fresca')
+    qtd_estoque = data.get('qtd_estoque')
+    preco = data.get('preco')    
     fruta = Fruta(nome=nome, classificacao=classificacao, fresca=fresca, qtd_estoque=qtd_estoque, preco=preco)
     fruta.save()
     print(fruta)
